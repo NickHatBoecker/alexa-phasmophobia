a03aea9f05f0ba6fabb172a52d15c6c7080daf0b
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-10-17 16:49:13 +0000

    updated lambda/skill.js.

---
+++ lambda/skill.js
@@ -0,0 +1,50 @@
+module.exports.ghosts = [
+    {
+        name: "Banshee",
+        nameResult: "Für die Banshee musst du das Geisterbuch, die Geisterkugeln und Gefriertemperaturen in das Journal eintragen.",
+    },
+    {
+        name: "Dämon",
+        nameResult: "Für den Dämon musst du das Geisterbuch, die Geisterkugeln und Gefriertemperaturen in das Journal eintragen.",
+    },
+    {
+        name: "Dschinn",
+        nameResult: "Für den Dschinn musst du das Geisterbuch, die Geisterkugeln und Gefriertemperaturen in das Journal eintragen.",
+    },
+    {
+        name: "Gespenst",
+        nameResult: "Für das Gespenst musst du das Geisterbuch, die Geisterkugeln und Gefriertemperaturen in das Journal eintragen.",
+    },
+    {
+        name: "Mare",
+        nameResult: "Für die Mare musst du das Geisterbuch, die Geisterkugeln und Gefriertemperaturen in das Journal eintragen.",
+    },
+    {
+        name: "Oni",
+        nameResult: "Für den Oni musst du das Geisterbuch, die Geisterkugeln und Gefriertemperaturen in das Journal eintragen.",
+    },
+    {
+        name: 'Phantom',
+        nameResult: 'Für das Phantom musst du das Geisterbuch, die Geisterkugeln und Gefriertemperaturen in das Journal eintragen.',
+    },
+    {
+        name: "Poltergeist",
+        nameResult: "Für den Poltergeist musst du das Geisterbuch, die Geisterkugeln und Gefriertemperaturen in das Journal eintragen.",
+    },
+    {
+        name: "Revenant",
+        nameResult: "Für den Revenant musst du das Geisterbuch, die Geisterkugeln und Gefriertemperaturen in das Journal eintragen.",
+    },
+    {
+        name: "Shade",
+        nameResult: "Für den Shade musst du das Geisterbuch, die Geisterkugeln und Gefriertemperaturen in das Journal eintragen.",
+    },
+    {
+        name: "Spirit",
+        nameResult: "Für den Spirit musst du das Geisterbuch, die Geisterkugeln und Gefriertemperaturen in das Journal eintragen.",
+    },
+    {
+        name: "Yurei",
+        nameResult: "Für die Yurei musst du das Geisterbuch, die Geisterkugeln und Gefriertemperaturen in das Journal eintragen.",
+    },
+];
